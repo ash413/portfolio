6ad174c79b6d365da42f63fd17bee4cd0e7e82a5
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-05-04 12:14:17 +0530

    base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import { Home, About, Skills, Projects, Contact } from './components'
+
+
 
 const App = () => {
+
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/skills' element={<Skills/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+    </div>
   )
 }
 
