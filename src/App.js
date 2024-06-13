1a5b7883109e4655d73286d7457adbb93fe82284
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-06-13 22:17:03 +0530

    correction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { Navbar, Home, Skills, Projects, Contact } from './components'
-import { DarkModeProvider } from './components/DarkModeContext'
 
 const App = () => {
 
   return (
     <div>
-      <DarkModeProvider >
-        <Navbar />
-        <Home />
-        <Projects />
-        <Skills />
-        <Contact />
-      </DarkModeProvider>
+      <Navbar />
+      <Home />
+      <Projects />
+      <Skills />
+      <Contact />
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,22 @@ import image from '../assets/profilepicture.jpg'
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
-import { DarkModeSwitch } from 'react-toggle-dark-mode';
-import { useDarkMode } from '../DarkModeContext';
+//import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 const Home = () => {
-  const { isDarkMode, toggleDarkMode } = useDarkMode();
+  //const { isDarkMode, toggleDarkMode } = useDarkMode();
 
   return (
     /*<div className='`mt-24 mb-4 flex justify-center ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`'>*/
-    <div className={`mt-24 mb-4 flex justify-center ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
+    <div className={`mt-24 mb-4 flex justify-center `}>
       <div className='mr-16 mt-20 '>
-        <p className='my-4 font-extrabold text-5xl'>VAISHNAVI KADAM</p>
-        <p className='my-4 font-bold text-2xl pl-12'>FRONT END DEVELOPER</p>
+        <p className='my-4 font-extrabold text-5xl pl-6'>VAISHNAVI KADAM</p>
+        <p className='my-4 font-bold text-2xl pl-20'>FRONT END DEVELOPER</p>
 
         <span className='my-6 ml-16 flex'>
-          <a href="https://twitter.com/vkadam" className='px-8'><FaSquareXTwitter /></a>
-          <a href="https://github.com/ash413" className='px-8'><FaGithub /></a>
-          <a href="https://www.linkedin.com/in/vaishnavikadam4/" className='px-8'><FaLinkedin /></a>
+          <a href="https://twitter.com/vkadam" className='px-8 '><FaSquareXTwitter size={32}/></a>
+          <a href="https://github.com/ash413" className='px-8'><FaGithub size={32}/></a>
+          <a href="https://www.linkedin.com/in/vaishnavikadam4/" className='px-8'><FaLinkedin size={32}/></a>
         </span>
 
         <p className='mt-4'>Final year engineering student that prioritizes results</p>
@@ -28,14 +27,6 @@ const Home = () => {
       </div>
       <div className='w-96 h-96'>
         <img src={image} alt="" className='w-full h-full object-cover rounded-full' />
-      </div>
-      <div>
-        <DarkModeSwitch
-          style={{ marginBottom: '2rem' }}
-          checked={isDarkMode}
-          onChange={toggleDarkMode}
-          size={50}
-        />
       </div>
     </div>
   )
